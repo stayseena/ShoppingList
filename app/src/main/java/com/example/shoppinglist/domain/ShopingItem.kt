ec206c0c382cb6ae327ce28653537a52ab01c673
Nastya Koryakina <stayseena@mail.ru>
2023-02-09 01:09:58 +0300

    create data class

--- app/src/main/java/com/example/shoppinglist/domain/ShopingItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopingItem.kt
@@ -1,8 +1,12 @@
 package com.example.shoppinglist.domain
 
 data class ShopingItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = -1
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
