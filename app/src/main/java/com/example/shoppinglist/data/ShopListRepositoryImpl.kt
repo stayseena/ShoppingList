a77def26165f23156b8024b6ff282ae9ab27841d
Nastya Koryakina <stayseena@mail.ru>
2023-02-10 14:52:41 +0300

    create presentation class

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,12 +1,21 @@
 package com.example.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopListRepository
 import com.example.shoppinglist.domain.ShopingItem
 
 object ShopListRepositoryImpl: ShopListRepository {
 
+    private val shopListLD = MutableLiveData<List<ShopingItem>>()
     private val shopList = mutableListOf<ShopingItem>()
     private var autoIncrementId = 0
+    init {
+        for (i in 0 until 10) {
+            val item = ShopingItem("Name $1", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun editShopItem(shopItem: ShopingItem) {
         val oldElement = getShopItem(shopItem.id)
@@ -16,6 +25,7 @@ object ShopListRepositoryImpl: ShopListRepository {
 
     override fun deleteShopItem(shopItem: ShopingItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun addShopItem(shopItem: ShopingItem) {
@@ -23,6 +33,7 @@ object ShopListRepositoryImpl: ShopListRepository {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun getShopItem(shopItemId: Int): ShopingItem {
@@ -31,7 +42,11 @@ object ShopListRepositoryImpl: ShopListRepository {
        } ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopingItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopingItem>> {
+        return shopListLD
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/GetShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoppinglist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
-    fun getShopItem(shopItemId : Int) {
+    fun getShopItem(shopItemId : Int): ShopingItem {
         return shopListRepository.getShopItem(shopItemId)
     }
 }
